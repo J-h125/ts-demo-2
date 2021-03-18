ecb6046c02464a149000f4691b81ae99fcc2b02b
JH <473731589@qq.com>
2021-03-18 21:19:11 +0800

    test1

--- test1.ts
+++ test1.ts
@@ -1,21 +1,67 @@
 #!/usr/bin/env ts-node
+
+function createPrefix(n){
+    return '--'.repeat(n)
+}
 {
-class Person{
-    public children : Person[];
-    constructor(public name,public age){
+    class Person{
+        children:Person[] = []
+        constructor(public name){
+        }
+        addChildren(child){
+             this.children.push(child)
+        }
+        introduce(n:number=1):void{
+            console.log(`${createPrefix(n-1)}:${this.name}`)
+            this.children.forEach((child)=>{
+                child.introduce(n+1)
+            })
+        }
+    }
+    let father = new Person('爸爸')
+    let son1 = new Person('儿子1')
+    let son2 = new Person('儿子1')
+    let son3 = new Person('儿子1')
+    let grandson1 = new Person('孙子1')
+    let grandson2 = new Person('孙子2')
 
-    }
-    sayHi(){
-        console.log('hello,my name is:'+this.name)
-    }
-    addChild(child:Person):void{
-        this.children.push(child)
-    }
+    father.addChildren(son1)
+    father.addChildren(son2)
+    father.addChildren(son3)
+    son1.addChildren(grandson1)
+    son2.addChildren(grandson2)
+    father.introduce()
 }
-let father = new Person('father',48)
-let son1 = new Person('son1',48)
-let son2 = new Person('son2',48)
 
-father.addChild(son1)
-father.addChild(son2)
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
